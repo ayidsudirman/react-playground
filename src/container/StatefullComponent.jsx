d40265d3bf5347605f75a165c7b8ea0d0685452a
ayidsudirman <ayidsudirman@protonmail.com>
2021-08-27 22:56:24 +0800

    first day

---
+++ src/container/StatefullComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StatefullComponent extends React.Component {
+    render(){
+        return <p>StateFull Component</p>
+    }
+}
+
+export default StatefullComponent;
